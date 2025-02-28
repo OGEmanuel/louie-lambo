73f346f46eeabae9f14562ad6ae598059d82f04c
OGEmanuel <1blacksanta416@gmail.com>
2025-02-28 08:58:07 +0100

    add:tier selector and accordion

--- app/miner/how-it-works.tsx
+++ app/miner/how-it-works.tsx
@@ -16,7 +16,62 @@ const HowItWorks = () => {
           How Lambo Miner works
         </AccordionTrigger>
         <AccordionContent className="p-4">
-          Yes. It adheres to the WAI-ARIA design pattern.
+          <ul className="flex list-inside list-decimal flex-col gap-4">
+            <li className="font-bold leading-[20.83px] text-[var(--color-black)] [&>span]:font-normal">
+              Deposit and Start Mining <br />
+              <span>
+                Users can begin mining by staking their $LAMBO tokens. Once
+                deposited, the system automatically detects their holdings and
+                assigns them to a mining tier. Mining begins instantly,
+                generating XRP rewards based on their staking duration and tier
+                level.
+              </span>
+            </li>
+            <li className="font-bold leading-[20.83px] text-[var(--color-black)] [&>span]:font-normal">
+              Fixed APY and Tier-Based Rewards <br />
+              <span>
+                The platform offers tier-based APY rewards, where higher $LAMBO
+                holdings and longer staking periods yield greater returns. APY
+                rates are structured as follows:
+              </span>
+              <ul className="ml-6 list-disc font-normal">
+                <li>Tier 1: 50% (7 days) to 1000% (6 months), max 250 XRP</li>
+                <li>Tier 2: 75% (7 days) to 1000% (6 months), max 500 XRP</li>
+                <li>Tier 3: 100% (7 days) to 1250% (6 months), max 1000 XRP</li>
+                <li>Tier 4: 200% (7 days) to 1500% (6 months), max 2000 XRP</li>
+              </ul>
+            </li>
+            <li className="font-bold leading-[20.83px] text-[var(--color-black)] [&>span]:font-normal">
+              Reinvestment Option <br />
+              <span>
+                Users can reinvest their earned XRP back into the mining pool to
+                maximize their rewards. Reinvesting resets the staking lock
+                timer, allowing for continuous reward compounding
+              </span>
+            </li>
+            <li className="font-bold leading-[20.83px] text-[var(--color-black)] [&>span]:font-normal">
+              Emergency Withdrawal Policy <br />
+              <ul className="ml-6 list-disc font-normal">
+                <li>
+                  Emergency Withdrawal Policy Users withdrawing before the end
+                  of their staking period will forfeit all earned XRP rewards.
+                </li>
+                <li>
+                  Additionally, 50% of their staked $LAMBO tokens will be lost
+                  as a penalty
+                </li>
+              </ul>
+            </li>
+            <li className="font-bold leading-[20.83px] text-[var(--color-black)] [&>span]:font-normal">
+              The platform provides real-time insights, including: <br />
+              <ul className="ml-6 list-disc font-normal">
+                <li>Current $LAMBO holdings</li>
+                <li>Staked and mined XRP balance</li>
+                <li>Live tracking of XRP being generated per second</li>
+                <li>Total XRP pooled and distributed.</li>
+              </ul>
+            </li>
+          </ul>
         </AccordionContent>
       </AccordionItem>
     </Accordion>
--- app/overview-main.tsx
+++ app/overview-main.tsx
@@ -1,16 +1,22 @@
 'use client';
 
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select';
-import { useState } from 'react';
+import { ReactNode, useState } from 'react';
 import RibbonFirst from './components/icons/ribbon-first';
 import Link from 'next/link';
 import { ChevronRight } from 'lucide-react';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import { Label } from '@/components/ui/label';
+import RibbonFirstSelect from './components/icons/ribbon-first-select';
+import { cn } from '@/lib/utils';
+import RibbonSecondSelect from './components/icons/ribbon-second-select';
+import RibbonThirdSelect from './components/icons/ribbon-third-select';
+import RibbonFourthSelect from './components/icons/ribbon-fourth-select';
+import ArrowDown from './components/icons/arrow-down';
 
 const OverviewMain = () => {
   return (
@@ -37,37 +43,45 @@ const OverviewMain = () => {
 export default OverviewMain;
 
 const TierSelector = () => {
-  const [value, setValue] = useState('tier-1');
+  const [value, setValue] = useState('Tier 1');
+  const [open, setOpen] = useState(false);
   return (
     <div className="flex flex-col gap-5 rounded-[18px] border border-[var(--color-stroke)] p-6">
-      <Select value={value} onValueChange={setValue}>
-        <SelectTrigger className="h-[46px] w-[147px] border-none p-0 text-2xl leading-[31.25.44px] text-[var(--color-black)] shadow-none focus:ring-0">
-          <SelectValue placeholder="Select a tier" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="tier-1">
-            <span className="flex items-center gap-[14px]">
-              <RibbonFirst />
-              Tier 1
-            </span>
-          </SelectItem>
-          <SelectItem value="tier-2">
-            <span className="flex items-center gap-[14px]">
-              <RibbonFirst />
-              Tier 2
-            </span>
-          </SelectItem>
-          <SelectItem value="tier-3">
-            <span className="flex items-center gap-[14px]">
-              <RibbonFirst />
-              Tier 3
-            </span>
-          </SelectItem>
-        </SelectContent>
-      </Select>
+      <Popover onOpenChange={setOpen} open={open}>
+        <PopoverTrigger className="flex w-max items-center gap-[11px]">
+          <span className="flex items-center gap-[14px]">
+            <RibbonFirst />
+            {value}
+          </span>
+          <ArrowDown />
+        </PopoverTrigger>
+        <PopoverContent className="w-full rounded-[18px] p-[42px]">
+          <RadioGroup
+            defaultValue={value}
+            onValueChange={setValue}
+            value={value}
+            className="grid grid-cols-2 gap-[42px]"
+          >
+            <TierItem tier="Tier 1" icon={<RibbonFirstSelect />} value={value}>
+              <Tier1 />
+            </TierItem>
+            <TierItem tier="Tier 2" icon={<RibbonSecondSelect />} value={value}>
+              <Tier2 />
+            </TierItem>
+            <TierItem tier="Tier 3" icon={<RibbonThirdSelect />} value={value}>
+              <Tier3 />
+            </TierItem>
+            <TierItem tier="Tier 4" icon={<RibbonFourthSelect />} value={value}>
+              <Tier4 />
+            </TierItem>
+          </RadioGroup>
+        </PopoverContent>
+      </Popover>
       <ul className="ml-2 list-inside list-disc leading-[20.83px] text-[var(--color-gray)]">
-        <li>250 maximum XRP mining</li>
-        <li>APY rates: 50%(7 days), 100%(7 months)</li>
+        {value === 'Tier 1' && <Tier1 />}
+        {value === 'Tier 2' && <Tier2 />}
+        {value === 'Tier 3' && <Tier3 />}
+        {value === 'Tier 4' && <Tier4 />}
       </ul>
     </div>
   );
@@ -130,5 +144,108 @@ const WalletSummary = () => {
         </div>
       </div>
     </div>
+  );
+};
+
+const TierItem = (props: {
+  tier: string;
+  icon: ReactNode;
+  children: ReactNode;
+  value: string;
+}) => {
+  return (
+    <div className="flex items-center">
+      <RadioGroupItem value={props.tier} id={props.tier} className="hidden" />
+      <Label
+        htmlFor={props.tier}
+        className={cn(
+          'flex flex-col gap-[19px] rounded-2xl border-2 p-4',
+          props.tier === props.value
+            ? 'border-[var(--color-lambo-green)]'
+            : 'border-[var(--color-stroke)]',
+        )}
+      >
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-3">
+            {props.icon}
+            <p className="text-[18px] leading-[23.44px] text-[var(--color-black)]">
+              {props.tier}
+            </p>
+          </div>
+          {props.tier === props.value && (
+            <p className="rounded-lg bg-[var(--color-lambo-black)] px-2 py-1 text-sm font-medium leading-[26px] text-[var(--color-lambo-green)]">
+              Current Tier
+            </p>
+          )}
+        </div>
+        <ul className="ml-2 list-inside list-disc leading-[26px] text-[var(--color-black)] [&>span]:font-medium">
+          {props.children}
+        </ul>
+      </Label>
+    </div>
+  );
+};
+
+const Tier1 = () => {
+  return (
+    <>
+      <li>
+        Hold <span>500K to 2M</span> $LAMBO
+      </li>
+      <li>
+        <span>250 XRP</span> maximum XRP mining
+      </li>
+      <li>
+        APY : 50% <span>(7 days) - 1000%</span> (6 months).
+      </li>
+    </>
+  );
+};
+
+const Tier2 = () => {
+  return (
+    <>
+      <li>
+        Hold <span>2.01M to 8M</span> $LAMBO
+      </li>
+      <li>
+        <span>500 XRP</span> maximum XRP mining
+      </li>
+      <li>
+        APY : 75% <span>(7 days) - 1000%</span> (6 months).
+      </li>
+    </>
+  );
+};
+
+const Tier3 = () => {
+  return (
+    <>
+      <li>
+        Hold <span>8.01M to 18M</span> $LAMBO
+      </li>
+      <li>
+        <span>500 XRP</span> maximum XRP mining
+      </li>
+      <li>
+        APY : 100% <span>(7 days) - 1250%</span> (6 months).
+      </li>
+    </>
+  );
+};
+
+const Tier4 = () => {
+  return (
+    <>
+      <li>
+        Hold <span>18M+</span> $LAMBO
+      </li>
+      <li>
+        <span>500 XRP</span> maximum XRP mining
+      </li>
+      <li>
+        APY : 200% <span>(7 days) - 1500%</span> (6 months).
+      </li>
+    </>
   );
 };
