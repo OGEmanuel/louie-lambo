3e9b8a3eb935a35fddc1095bea7f3e0315be4f7c
daezy <ezetdaniel@gmail.com>
2025-03-10 07:04:29 +0100

    created mine function

--- app/referral.tsx
+++ app/referral.tsx
@@ -1,20 +1,23 @@
 'use client';
 
-import { ButtonLoading } from '@/components/ui/button-loading';
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
-} from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
-import { zodResolver } from '@hookform/resolvers/zod';
+// import { ButtonLoading } from '@/components/ui/button-loading';
+// import {
+//   Form,
+//   FormControl,
+//   FormField,
+//   FormItem,
+//   FormMessage,
+// } from '@/components/ui/form';
+// import { Input } from '@/components/ui/input';
+import { AppContext } from '@/context/AppContext';
+// import { zodResolver } from '@hookform/resolvers/zod';
 import { Copy } from 'lucide-react';
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
+import { useContext } from 'react';
+// import { useForm } from 'react-hook-form';
+// import { z } from 'zod';
 
 export const Referral = () => {
+  const appContext = useContext(AppContext);
   return (
     <>
       <div className="flex flex-col gap-[46px]">
@@ -26,13 +29,12 @@ export const Referral = () => {
             Earn free XRP bonus from referrals
           </p>
         </div>
-        <ReferralForm />
       </div>
       <div className="flex flex-col gap-[13px] leading-[20.83px]">
         <p className="text-[var(--text-black)]">Referral link</p>
         <div className="flex max-2xl:flex-col max-2xl:gap-2 max-xl:flex-row max-xl:items-center max-xl:justify-between 2xl:items-center 2xl:justify-between">
           <p className="text-[var(--color-gray)] max-2xl:line-clamp-1 max-2xl:text-ellipsis">
-            https://xrpthestan.....34xcvsk5
+            http://localhost:3000?ref={appContext.walletAddress}
           </p>
           <div className="flex items-center gap-1 text-[var(--color-black)]">
             <Copy />
@@ -44,59 +46,59 @@ export const Referral = () => {
   );
 };
 
-const ReferralFormSchema = z.object({
-  email: z
-    .string({
-      required_error: 'Please select an email to display.',
-    })
-    .email(),
-});
+// const ReferralFormSchema = z.object({
+//   email: z
+//     .string({
+//       required_error: 'Please select an email to display.',
+//     })
+//     .email(),
+// });
 
-const ReferralForm = () => {
-  const form = useForm<z.infer<typeof ReferralFormSchema>>({
-    resolver: zodResolver(ReferralFormSchema),
-    defaultValues: {
-      email: '',
-    },
-  });
+// const ReferralForm = () => {
+//   const form = useForm<z.infer<typeof ReferralFormSchema>>({
+//     resolver: zodResolver(ReferralFormSchema),
+//     defaultValues: {
+//       email: '',
+//     },
+//   });
 
-  function onSubmit(data: z.infer<typeof ReferralFormSchema>) {
-    // toast({
-    //   title: "You submitted the following values:",
-    //   description: (
-    <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-      <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-    </pre>;
-    //   ),
-    // })
-  }
+//   function onSubmit(data: z.infer<typeof ReferralFormSchema>) {
+//     // toast({
+//     //   title: "You submitted the following values:",
+//     //   description: (
+//     <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+//       <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+//     </pre>;
+//     //   ),
+//     // })
+//   }
 
-  return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input
-                  placeholder="Enter email address"
-                  {...field}
-                  className="h-[49px] w-full rounded-[10px] p-[14px] text-base leading-[20.83px] shadow-none placeholder:text-[var(--color-gray)]"
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <ButtonLoading
-          type="submit"
-          label="Refer"
-          className="w-full text-base font-normal leading-[20.83px] max-lg:h-[49px]"
-          isPending={false}
-        />
-      </form>
-    </Form>
-  );
-};
+//   return (
+//     <Form {...form}>
+//       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
+//         <FormField
+//           control={form.control}
+//           name="email"
+//           render={({ field }) => (
+//             <FormItem>
+//               <FormControl>
+//                 <Input
+//                   placeholder="Enter email address"
+//                   {...field}
+//                   className="h-[49px] w-full rounded-[10px] p-[14px] text-base leading-[20.83px] shadow-none placeholder:text-[var(--color-gray)]"
+//                 />
+//               </FormControl>
+//               <FormMessage />
+//             </FormItem>
+//           )}
+//         />
+//         <ButtonLoading
+//           type="submit"
+//           label="Refer"
+//           className="w-full text-base font-normal leading-[20.83px] max-lg:h-[49px]"
+//           isPending={false}
+//         />
+//       </form>
+//     </Form>
+//   );
+// };
---
+++ lib/xrp/depositXrp.ts
@@ -0,0 +1,21 @@
+import { XummSdk } from 'xumm-sdk';
+const xumm = new XummSdk(process.env.XUMM_API_KEY, process.env.XUMM_API_SECRET);
+
+export async function depositXrpWithXumm(
+  userAddress: string,
+  amount: number,
+  poolAddress: string,
+) {
+  const signRequest = await xumm.payload.create(
+    {
+      TransactionType: 'Payment',
+      Account: userAddress,
+      Destination: poolAddress,
+      Amount: (amount * 1000000).toString(),
+    },
+    true,
+  );
+
+  if (!signRequest) throw new Error('could not create payload');
+  return signRequest;
+}
