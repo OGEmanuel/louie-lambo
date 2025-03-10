cc3eefd7677c52d992d9ff7c4ddc38cfff9faf6f
OGEmanuel <1blacksanta416@gmail.com>
2025-03-10 15:01:52 +0100

    add:calculator feature in progress

--- app/calculator/form.tsx
+++ app/calculator/form.tsx
@@ -1,14 +1,28 @@
 'use client';
 
 import { ButtonLoading } from '@/components/ui/button-loading';
-import { Form, FormField } from '@/components/ui/form';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
 import NumberInput from '@/components/ui/number-input';
 import RadioInput from '@/components/ui/radio-input';
-import { AppContext } from '@/context/AppContext';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useContext, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
+import axios from 'axios';
 
 const FormSchema = z.object({
   amount: z
@@ -34,25 +48,49 @@ const FormSchema = z.object({
   duration: z.string().min(2, {
     message: 'Duration must be at least 2 characters.',
   }),
+  tier: z.string().min(1, {
+    message: 'Tier must be at least 1 character.',
+  }),
 });
 
+type Tier = {
+  _id: string;
+  name: string;
+  description: string;
+  oneWeekApy: number;
+  twoWeeksApy: number;
+  oneMonthApy: number;
+  threeMonthsApy: number;
+  sixMonthsApy: number;
+  createdAt: string; // ISO date string
+  updatedAt: string; // ISO date string
+  __v: number;
+};
+
 const CalculatorForm = () => {
+  const [tiers, setTiers] = useState<Tier[]>([]);
+
   useEffect(() => {
-    const tiers = fetch('api/admin/getTiers');
-    tiers
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-      });
+    const fetchTiers = async () => {
+      try {
+        const response = await axios.get('/api/admin/getTiers');
+        setTiers(response.data.tiers);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchTiers();
   }, []);
 
-  // console.log(appContext);
+  console.log(tiers);
 
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
       amount: 0,
-      duration: '14-days',
+      duration: 'twoWeeks',
+      tier: '0',
     },
   });
 
@@ -80,6 +118,36 @@ const CalculatorForm = () => {
         </p>
         <FormField
           control={form.control}
+          name="tier"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-lg font-medium leading-[23.44px] text-[var(--color-black)] max-lg:text-base">
+                Select Tier
+              </FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger className="text-[var(--color-black)]">
+                    <SelectValue placeholder="Select a tier" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {Array.isArray(tiers) && tiers.length > 0 ? (
+                    tiers.map((tier, i) => (
+                      <SelectItem key={tier._id} value={i.toString()}>
+                        {tier.name}
+                      </SelectItem>
+                    ))
+                  ) : (
+                    <p className="px-2 text-gray-500">No tiers available</p>
+                  )}
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
           name="amount"
           render={({ field }) => (
             <NumberInput
@@ -95,13 +163,13 @@ const CalculatorForm = () => {
           render={({ field }) => (
             <RadioInput
               label="Duration"
-              description="Projected yield: APY 4.5%"
+              description={`Projected yield: APY ${field.value === 'oneWeek' ? tiers[Number(form.watch('tier'))].oneWeekApy : field.value === 'twoWeeks' ? tiers[Number(form.watch('tier'))].twoWeeksApy : field.value === 'oneMonth' ? tiers[Number(form.watch('tier'))].oneMonthApy : field.value === 'threeMonths' ? tiers[Number(form.watch('tier'))].threeMonthsApy : tiers[Number(form.watch('tier'))].sixMonthsApy}%`}
               options={[
-                { label: '7 days', value: '7-days' },
-                { label: '14 days', value: '14-days' },
-                { label: '1 month', value: '1-month' },
-                { label: '3 months', value: '3-months' },
-                { label: '6 months', value: '6-months' },
+                { label: '7 days', value: 'oneWeek' },
+                { label: '14 days', value: 'twoWeeks' },
+                { label: '1 month', value: 'oneMonth' },
+                { label: '3 months', value: 'threeMonths' },
+                { label: '6 months', value: 'sixMonths' },
               ]}
               field={field}
             />
