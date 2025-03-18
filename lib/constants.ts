d6bce2fda7f7679549c8355154e36aedfac1769a
daezy <ezetdaniel@gmail.com>
2025-03-18 12:06:58 +0100

    updated tiers

--- lib/constants.ts
+++ lib/constants.ts
@@ -7,7 +7,7 @@ export const LAMBO_TOKEN_CODE = '4C414D424F000000000000000000000000000000';
 export const LAMBO_TOKEN_ISSUER = 'rJXsaaTBAqZRHeLLd13TpbZhraJnGunAvW';
 
 export interface Tier {
-  name: 'T1 Max' | 'T2 Max' | 'T3 Max' | 'T4 Max';
+  name: 'T1' | 'T2' | 'T3' | 'T4' | 'T5';
   description: string;
   minimumTokensHeld: number;
   maximumTokensHeld: number;
@@ -21,53 +21,67 @@ export interface Tier {
 
 export const tiers: Tier[] = [
   {
-    name: 'T1 Max',
+    name: 'T1',
     description:
-      'Holders of 500K to 2M $LAMBO can mine a maximum of 250 XRP. APY rates range from 50% (7 days) to 1000% (6 months).',
-    minimumTokensHeld: 500_000,
-    maximumTokensHeld: 4_000_000,
+      '🐠 up to 200 xrp per wallet mined All holders between 1M-2.99M $LAMBO',
+    minimumTokensHeld: 1_000_000,
+    maximumTokensHeld: 2_999_999,
+    oneWeekApy: 25,
+    twoWeeksApy: 50,
+    oneMonthApy: 100,
+    threeMonthsApy: 300,
+    sixMonthsApy: 500,
+    maxXrpMineable: 200,
+  },
+  {
+    name: 'T2',
+    description:
+      '🐬 up to 400 xrp per wallet mined All holders between 3M-5.99M $LAMBO',
+    minimumTokensHeld: 3_000_000,
+    maximumTokensHeld: 5_999_999,
     oneWeekApy: 50,
     twoWeeksApy: 100,
     oneMonthApy: 200,
-    threeMonthsApy: 500,
-    sixMonthsApy: 1000,
-    maxXrpMineable: 250,
+    threeMonthsApy: 400,
+    sixMonthsApy: 750,
+    maxXrpMineable: 400,
   },
   {
-    name: 'T2 Max',
+    name: 'T3',
     description:
-      'Holders of 2.01M to 8M $LAMBO can mine a maximum of 500 XRP. APY rates range from 75% (7 days) to 1000% (6 months)',
-    minimumTokensHeld: 4_000_000,
-    maximumTokensHeld: 9_900_000,
+      '🦈 up to 600 xrp per wallet mined All holders between 6M-11.99M $LAMBO',
+    minimumTokensHeld: 6_000_000,
+    maximumTokensHeld: 11_999_999,
     oneWeekApy: 75,
     twoWeeksApy: 150,
-    oneMonthApy: 300,
+    oneMonthApy: 250,
     threeMonthsApy: 500,
     sixMonthsApy: 1000,
-    maxXrpMineable: 500,
+    maxXrpMineable: 600,
   },
   {
-    name: 'T3 Max',
+    name: 'T4',
     description:
-      'Holders of 10M-19.9M $LAMBO $LAMBO can mine a maximum of 1000 XRP. APY rates range from 100% (7 days) to 1250% (6 months)',
-    minimumTokensHeld: 10_000_000,
-    maximumTokensHeld: 19_900_000,
+      '🐋 up to 1,000 xrp per wallet mined All holders between 12M-19.99M $LAMBO',
+    minimumTokensHeld: 12_000_000,
+    maximumTokensHeld: 19_999_999,
     oneWeekApy: 100,
     twoWeeksApy: 200,
-    oneMonthApy: 400,
-    threeMonthsApy: 600,
+    oneMonthApy: 300,
+    threeMonthsApy: 750,
     sixMonthsApy: 1250,
-    maxXrpMineable: 750,
+    maxXrpMineable: 1000,
   },
+
   {
-    name: 'T4 Max',
+    name: 'T5',
     description:
-      'Holders of 20M+ $LAMBO can mine a maximum of 2000 XRP. APY rates range from 200% (7 days) to 1500% (6 months).',
+      '🐳 up to 2000 xrp per wallet mined All holders above 20M $LAMBO',
     minimumTokensHeld: 20_000_000,
     maximumTokensHeld: Infinity,
-    oneWeekApy: 150,
-    twoWeeksApy: 250,
-    oneMonthApy: 400,
+    oneWeekApy: 200,
+    twoWeeksApy: 300,
+    oneMonthApy: 500,
     threeMonthsApy: 1000,
     sixMonthsApy: 1500,
     maxXrpMineable: 2000,
