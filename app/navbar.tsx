c5db14a50eaa312b4d3152a259b0b5e7ab2b4c5f
daezy <ezetdaniel@gmail.com>
2025-04-02 23:41:26 +0100

    fix

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import LamboLogo from './components/icons/lambo-logo';
+// import LamboLogo from './components/icons/lambo-logo';
 import LamboLogoSmall from './components/icons/lambo-logo-mobile';
 import { MobileSidenav } from './sidenav';
 import Link from 'next/link';
